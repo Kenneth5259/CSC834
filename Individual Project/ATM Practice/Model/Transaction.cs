63c52184f68bf9950941d2ffb2d72f4d3b3c66a8
Kenneth Carroll <Kenneth.James.Carroll@gmail.com>
2021-04-25 11:10:22 -0400

    Moved Views to View Folder, added Transaction Base Class

---
+++ Individual Project/ATM Practice/Model/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ATM_Practice.Model
+{
+    class Transaction
+    {
+        public int accountNum { get; set; }
+        public DateTime date { get; set; }
+        public int transactionNum {  get; }
+
+        public string transactionType { get; set; }
+        public int toAccount { get; set; }
+        public int fromAccount { get; set; }
+
+    }
+}
