e6b5c216a5808bf480dfecd926f9c1433a9697f2
Kenneth Carroll <Kenneth.James.Carroll@gmail.com>
2021-05-01 16:01:51 -0400

    Begin implementing state diagram for login

--- Individual Project/ATM Practice/Program.cs
+++ Individual Project/ATM Practice/Program.cs
@@ -19,7 +19,7 @@ namespace ATM_Practice
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainMenuForm());
+            Application.Run(new LogInScreen());
         }
     }
 }
--- Individual Project/ATM Practice/View/LogInScreen.cs
+++ Individual Project/ATM Practice/View/LogInScreen.cs
@@ -13,6 +13,22 @@ namespace ATM_Practice
         public LogInScreen()
         {
             InitializeComponent();
+
+            // simulate the time to insert a card
+            System.Threading.Tasks.Task.Delay(new TimeSpan(0, 0, 5)).ContinueWith(o => { this.simulateCardInsert(); });
+        }
+        private void simulateCardInsert()
+        {
+            // Use method invoker to avoid cross thread exception
+            this.Invoke(new MethodInvoker(delegate ()
+            {
+                // make the pin entry visible 
+                LoginScreenPinEntryTableLayoutPanel.Visible = true;
+
+                // hide the card insertion prompt
+                LoginScreenInsertCardTableLayoutPanel.Visible = false;
+            }));
+
         }
     }
 }
